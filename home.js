edc78ed9852d75dc2447bdd33512af5e513b18d0
Nikhil <n.s.r@Nikhils-MacBook-Air.local>
2025-01-10 01:00:42 +0530

    learning more concepts

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import img from '/hi.png';
+import img2 from '/leet.png'
 const Home = () => {
   const [displayText, setDisplayText] = useState("");
   const [index, setIndex] = useState(0); 
@@ -27,9 +28,15 @@ const Home = () => {
       <h1 className="yo">I am a Frontend Developer</h1>
       </div>
       <div>
-      <img src={img} alt="Profile photo" />
+      <img className="in" src={img} alt="Profile photo" />
       </div>
     </div>
+    {/* <div className="hilo">
+      <a  href="https://www.linkedin.com/in/nikhil-singh-rathore-983b8532b/"><i class="fa-brands fa-linkedin"></i></a>
+      <a  href="https://github.com/nikhilsr20"><i class="fa-brands fa-github"></i></a>
+      <a href="https://leetcode.com/u/nikhil_singh_rathore________/"><img  src={img2} /></a>
+    </div> */}
+
     
     </>
   );
